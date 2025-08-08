d8f9c007f287d7f7cf0dc2c94ff48ac45eaf843f
nasirjan244888-lab <nasirjan244888@gmail.com>
2025-08-08 13:26:08 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "",
+    SESSION_ID: process.env.SESSION_ID || "drakonis~F2FiFIxA#Xg-uQq5ZZjVJxGKLRb4OZTyxjpGMvbqN-bFf6DQIzxc",
     // add your Session Id make sure it starts with kirito~
 
     PREFIX: process.env.PREFIX || ".",
